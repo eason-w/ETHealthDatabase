4fdc3f0f684ddf5630bed01584db2871a89d27c5
eason-w <65966679+eason-w@users.noreply.github.com>
2021-10-15 09:03:53 -0400

    Update dataviewing.sol

--- dataviewing.sol
+++ dataviewing.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
+import "./datacreation.sol";
+
 contract DataViewing is HealthDatabase {
 
     function viewData(uint personId) external view returns(struct[] storage) {
